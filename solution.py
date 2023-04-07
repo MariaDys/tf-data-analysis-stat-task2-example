835285fb684cf46ef102a2638eeebfbb31931c97
MariaDys <77514289+MariaDys@users.noreply.github.com>
2023-04-07 12:24:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import t
+from scipy.stats import chi2
 
 
 chat_id = 734920047 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    x_mean = x.mean()
-    s = np.sqrt(np.sum((x - x_mean)**2) / (n - 1))
     alpha = 1 - p
-    t_alpha_2 = t.ppf(alpha/2, n-1)
-    t_1_alpha_2 = t.ppf(1-alpha/2, n-1)
-    left = x_mean - s * t_1_alpha_2 / np.sqrt(n)
-    right = x_mean - s * t_alpha_2 / np.sqrt(n)
-    return (left, right)
+    loc = x.mean()
+    scale = np.sqrt(np.var(x) * 22 / n)
+    chi2_left = chi2.ppf(alpha / 2, df=n-1)
+    chi2_right = chi2.ppf(1 - alpha / 2, df=n-1)
+    return np.sqrt(((n - 1) * np.var(x)) / chi2_right), \
+           np.sqrt(((n - 1) * np.var(x)) / chi2_left)
